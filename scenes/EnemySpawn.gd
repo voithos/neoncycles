42baa0386abe179fcf22e9b70cd21097656549cb
Zaven Muradyan <zaven@voithos.io>
2021-08-23 00:42:55 -0700

    Add spawn rate speedup.

--- scenes/EnemySpawn.gd
+++ scenes/EnemySpawn.gd
@@ -3,9 +3,14 @@ extends Spatial
 export (BoxShape) var spawn_shape: BoxShape
 export (float) var spawn_rate = 1.0 # per second
 export (float) var spawn_sigma = 0.5
+export (float) var spawn_rate_speedup_time = 0 # seconds
+export (float) var spawn_rate_max_speedup = 1.0 # no speedup
 export (bool) var is_active = true
 export (PackedScene) var enemy = preload("res://scenes/Drone.tscn")
 export (bool) var debug = false
+
+var speedup_timer = 0
+var internal_spawn_rate = spawn_rate
 
 var timer = 0
 var next_tick = 0
@@ -35,6 +40,12 @@ func _physics_process(delta):
 
         timer = 0
         _set_next_tick()
+        
+    speedup_timer += delta
+    if spawn_rate_speedup_time != 0:
+        var new_spawn_rate = range_lerp(speedup_timer, 0, spawn_rate_speedup_time, spawn_rate, spawn_rate / spawn_rate_max_speedup)
+        # Don't go overboard
+        internal_spawn_rate = clamp(new_spawn_rate, spawn_rate / spawn_rate_max_speedup, spawn_rate)
 
 func _set_next_tick():
-    next_tick = spawn_rate + rand_range(-1, 1) * spawn_sigma
+    next_tick = internal_spawn_rate + rand_range(-1, 1) * spawn_sigma
